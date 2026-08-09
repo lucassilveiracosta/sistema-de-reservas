e0ff31e9d4578ccadd6796681164f34a37d56f0b
lucassilveiracosta <lscosta2007@gmail.com>
2026-08-09 11:19:35 -0300

    refactor(app): register application modules

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PrismaService } from './prisma/prisma.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { UserModule } from './user/user.module';
+import { RoomModule } from './room/room.module';
+import { ReservationModule } from './reservation/reservation.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, UserModule, RoomModule, ReservationModule],
   controllers: [AppController],
-  providers: [AppService, PrismaService],
+  providers: [AppService],
 })
 export class AppModule {}
