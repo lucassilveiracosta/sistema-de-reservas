783089b9011f60f7fc77f650c206f50e589baccd
lucassilveiracosta <lscosta2007@gmail.com>
2026-08-10 22:09:49 -0300

    chore(docs): setup swagger module in main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  const config = new DocumentBuilder()
+    .setTitle('Seed a Bit - Sistema de Reserva de Salas')
+    .setDescription('API para gerenciamento de salas e reservas com proteção JWT e regras de conflito.')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+    
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
