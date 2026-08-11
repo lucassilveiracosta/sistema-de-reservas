f30d2b821957310de5ece225db7fcbdff5bca590
lucassilveiracosta <lscosta2007@gmail.com>
2026-08-11 10:49:14 -0300

    fix(core): load environment variables from .env to respect PORT

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,9 @@
+import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { env } from 'process';
+import { link } from 'fs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -16,5 +19,6 @@ async function bootstrap() {
   SwaggerModule.setup('api/docs', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
+  console.log("API running on: https://localhost:" + process.env.PORT + "/");
 }
 bootstrap();
