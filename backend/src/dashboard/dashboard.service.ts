5dde1c1781ff63fe3254da171dd486ffb9e1ab1b
lucassilveiracosta <lscosta2007@gmail.com>
2026-08-18 19:57:42 -0300

    feat: new endpoint to dashboard, totalUsers

--- backend/src/dashboard/dashboard.service.ts
+++ backend/src/dashboard/dashboard.service.ts
@@ -1,18 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
-import { ReservationStatus } from '@prisma/client';
+import { ReservationStatus, Role } from '@prisma/client';
 import { jwtSecret } from 'src/auth/jwt.strategy';
 
 @Injectable()
 export class DashboardService {
   constructor(private prisma: PrismaService) {}
-  
+
   async getStatistics() {
     const totalRooms = await this.prisma.room.count({ where: { isActive: true } });
     
     const activeReservations = await this.prisma.reservation.count({ where: { status: ReservationStatus.ACTIVE } });
     const completedReservations = await this.prisma.reservation.count({ where: { status: ReservationStatus.COMPLETED } });
     const cancelledReservations = await this.prisma.reservation.count({ where: { status: ReservationStatus.CANCELLED } });
+    const totalUsers = await this.prisma.user.count( { where: { role: Role.USER } });
 
     const upcomingReservations = await this.prisma.reservation.findMany({
       where: {
@@ -33,7 +34,8 @@ export class DashboardService {
           completed: completedReservations,
           cancelled: cancelledReservations,
           total: activeReservations + completedReservations + cancelledReservations
-        }
+        },
+        totalUsers: totalUsers
       },
       upcomingReservations
     };
